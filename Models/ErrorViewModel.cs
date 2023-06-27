036bf0057ac0e7ef01555a4cef40c94783cddc90
Shimon <sh0527691229@gmail.com>
2023-06-27 18:08:21 +0300

    first

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NessClientsServer.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
