dd97025ee41643b514b071e2475a38c0a0a4c6b1
Shimon <sh0527691229@gmail.com>
2023-06-28 19:37:50 +0300

    add client id validation

---
+++ wwwroot/js/newClient.js
@@ -0,0 +1,44 @@
+﻿(function(){
+
+    const is_israeli_id_number= function(id) {
+        id = String(id).trim();
+        if (id.length > 9 || isNaN(id)) return false;
+        id = id.length < 9 ? ("00000000" + id).slice(-9) : id;
+            return Array.from(id, Number).reduce((counter, digit, i) => {
+                const step = digit * ((i % 2) + 1);
+                return counter + (step > 9 ? step - 9 : step);
+            }) % 10 === 0;
+    }
+    
+    const trimInputs = function(form){
+        [...form.getElementsByTagName("input")].map(input => input.value = input.value.trim())
+    };
+
+    document.addEventListener('DOMContentLoaded', () => {
+        let f = document.getElementById("formNewClientDetails");
+        f.addEventListener('submit',(e)=>{
+            e.preventDefault();
+            trimInputs(f);
+            let id = document.getElementById('id')
+            if(!is_israeli_id_number(id.value))
+            {
+                id.setCustomValidity("Bad check digit");
+                id.reportValidity();
+            }
+            else
+                f.onsubmit();
+            })
+        })
+
+    })
+();
+
+
+
+
+
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+console.log("shimon won")
