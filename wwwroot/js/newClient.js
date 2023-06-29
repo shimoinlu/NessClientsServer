03d885738b8fce1efa71dec761b18f5bf0ce40c6
Shimon <sh0527691229@gmail.com>
2023-06-29 13:23:15 +0300

    fix some validation function

--- wwwroot/js/newClient.js
+++ wwwroot/js/newClient.js
@@ -11,16 +11,29 @@
     }
     
     const trimInputs = function(form){
+
         [...form.getElementsByTagName("input")].map(input => input.value = input.value.trim())
     };
-
+    const ValidateOneInput = function(i){
+        if(i.valid){
+            return true
+        }
+        else{
+            i.reportValidity();
+            return false;    
+        }
+    }
+    const ValidateAllInputs = function(){
+        const allInputs = [...form.getElementsByTagName("input")]
+        return allInputs.every(ValidateOneInput);
+    }
     document.addEventListener('DOMContentLoaded', () => {
         let f = document.getElementById("formNewClientDetails");
         let buttonSubmit = document.getElementById("buttonSubmit");
         buttonSubmit.addEventListener('click',(e)=>{
             trimInputs(f);
             let id = document.getElementById('id')
-            if(!is_israeli_id_number(id.value))
+            if(!is_israeli_id_number(id.value) && ValidateAllInputs())
             {
                 id.setCustomValidity("Bad check digit");
                 id.reportValidity();
